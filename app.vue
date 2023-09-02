0d16a45e02541ea632bba6fb0d5965da58b007b3
airblack0012 <airblack0012@gmail.com>
2023-09-02 22:54:59 +0300

    Проверка асинхронной загрузки

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheBanner from '../components/TheBanner.vue';
+import {lazyLoadComponentIfVisible} from './utils'
+const LazyLoaded = lazyLoadComponentIfVisible({
+	componentLoader: () => import('~/components/HelloWorld.vue'),
+	loadingComponent: TheBanner,
+});
+//props
+//computed
+//data
+//methods
+</script>
+<template>
+	<div>
+		<LazyLoaded/>
+	</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/TheBanner.vue
@@ -0,0 +1,16 @@
+<script setup="">
+//props
+//computed
+//data
+//methods
+</script>
+<template>
+	<div class="banner">
+		Banner
+	</div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
